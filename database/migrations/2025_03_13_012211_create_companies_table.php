14c09fdbb4fa305837d2481258470b3736f4cbf9
John Doe <johndoe@example.com>
2025-03-13 12:29:47 +0100

    create users & companies migrations

--- database/migrations/2025_03_13_012211_create_companies_table.php
+++ database/migrations/2025_03_13_012211_create_companies_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -13,10 +11,15 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('company_name');
+            $table->string('address');
+            $table->json('legal_documents')->nullable();
+            $table->boolean('is_validated')->default(false);
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
         });
     }
-
     /**
      * Reverse the migrations.
      */
